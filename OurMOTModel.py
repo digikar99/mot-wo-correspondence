798f0171b97f4ceb5c1e5197994730c9fa89b8af
digikar99 <shubhamkarayare@gmail.com>
2022-10-15 10:01:33 +0530

    [change] don't treat zero targets case specially

--- OurMOTModel.py
+++ OurMOTModel.py
@@ -58,6 +58,7 @@ class OurMOTModel:
 			self.target_locations = new_target_locations
 		elif strategy == "lowest":
 			target_locations = self.target_locations
+			if len(target_locations) == 0: return
 			update_idx = self.update_idx
 			loc = self.target_locations[update_idx]
 			i, j = loc
@@ -74,20 +75,25 @@ class OurMOTModel:
 					# TODO: Incorporate two different parameters for tracking vs ID update
 					# if self.num_updates % 2 == 0:
 					# should_update_id = (self.num_updates % len(target_locations) < 2)
-					should_update_id = True
+					# should_update_id = True
+					should_update_id = False
+					# should_update_id = (random.random() < 0.5)
 					# should_update_id = (self.num_updates % 5 < 3)
 					# should_update_id = (self.num_updates % len(target_locations) < (7 - len(target_locations)))
-					# should_update_id = (self.num_updates % 2 == 1)
-					# should_update_id = (random.random() < 0.5)
+					# should_update_id = (self.num_updates % 2 == 0)
+					# should_update_id = (random.random() < (1 - len(target_locations)/12))
 					new_idx = update_idx+1
 					while new_idx < len(target_locations):
 						if new_target_locations[update_idx] > new_target_locations[new_idx]:
 							del new_target_locations[update_idx]
 							new_target_locations.insert(new_idx, newloc)
+							idx = self.target_id_sequence[update_idx]
+							del self.target_id_sequence[update_idx]
 							if should_update_id:
-								idx = self.target_id_sequence[update_idx]
-								del self.target_id_sequence[update_idx]
 								self.target_id_sequence.insert(new_idx, idx)
+							else:
+								self.target_id_sequence.insert(update_idx, idx)
+								# self.target_id_sequence.insert(new_idx, None)
 							break
 						new_idx += 1
 					new_idx = 0
@@ -95,10 +101,13 @@ class OurMOTModel:
 						if new_target_locations[update_idx] < new_target_locations[new_idx]:
 							del new_target_locations[update_idx]
 							new_target_locations.insert(new_idx, newloc)
+							idx = self.target_id_sequence[update_idx]
+							del self.target_id_sequence[update_idx]
 							if should_update_id:
-								idx = self.target_id_sequence[update_idx]
-								del self.target_id_sequence[update_idx]
 								self.target_id_sequence.insert(new_idx, idx)
+							else:
+								self.target_id_sequence.insert(update_idx, idx)
+								# self.target_id_sequence.insert(new_idx, None)
 							break
 						new_idx += 1
 				else:
@@ -119,7 +128,7 @@ class OurMOTModel:
 				format(target_locations, new_target_locations, dist)
 			self.target_locations = new_target_locations
 			self.update_idx += 1
-			self.update_idx %= len(self.target_locations)
+			if len(self.target_locations): self.update_idx %= len(self.target_locations)
 		else:
 			raise Exception("Unknown strategy: " + strategy)
 
@@ -139,8 +148,8 @@ class OurMOTModel:
 				attended_locations.append(nearest_object_loc)
 			elif dist < nearest_object_bound:
 				attended_locations.append(nearest_object_loc)
-			else:
-				attended_locations.append(random.choice(object_locations))
+			# else:
+			# 	attended_locations.append(random.choice(object_locations))
 
 		if num_locations is None or len(attended_locations) >= num_locations:
 			return attended_locations
@@ -158,18 +167,23 @@ class OurMOTModel:
 		attended_locations = sorted(attended_locations)
 		target_id_sequence = self.target_id_sequence
 		for (i, loc) in enumerate(attended_locations):
+			if target_id_sequence[i] is None: continue
 			target_location_id_map[loc] = target_id_sequence[i]
 		if env is None or num_targets is None:
 			return target_location_id_map
 		else:
+			known_ids = [_id for _id in self.target_id_sequence if _id is not None]
+			all_ids   = list(range(0, num_targets))
+			unknown_ids = list(set(all_ids) - set(known_ids))
+			random.shuffle(unknown_ids)
+			num_guesses = len(unknown_ids)
+
 			locations         = env.get_object_locations()
-			known_locations   = attended_locations
-			unknown_locations = list(set(locations) - set(known_locations))
-			num_guesses = num_targets - len(known_locations)
-			random.shuffle(unknown_locations)
-			index = len(target_location_id_map)
+			known_id_locations   = target_location_id_map.keys()
+			unknown_id_locations = list(set(locations) - set(known_id_locations))
+			random.shuffle(unknown_id_locations)
 			for i in range(num_guesses):
-				loc = unknown_locations[i]
+				loc   = unknown_id_locations[i]
+				index = unknown_ids[i]
 				target_location_id_map[loc] = index
-				index += 1
 			return target_location_id_map
